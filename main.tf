07652d3e6022ffb12a3811cac4aa90517a72644a
phamngocdat <Ngocdat199614@gmail.com>
2023-12-28 11:05:37 +0700

    first commit

---
+++ main.tf
@@ -0,0 +1,21 @@
+# TODO: Designate a cloud provider, region, and credentials
+# TODO: provision 4 AWS t2.micro EC2 instances named Udacity T2
+resource "aws_instance" "Udacity_T2" {
+  count = "4"
+  ami = "ami-053b0d53c279acc90"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "Udacity T2"
+  }
+}
+
+# TODO: provision 2 m4.large EC2 instances named Udacity M4
+# Comment code to destroy
+resource "aws_instance" "Udacity_M4" {
+  count = "2"
+  ami = "ami-053b0d53c279acc90"
+  instance_type = "m4.large"
+  tags = {
+   Name = "Udacity M4"
+ }
+}
